593d59b0148b39d3179c7bff59d96a060c4898c5
“Lissa <lwarshaw674@gmail.com>
2023-04-28 12:51:46 -0400

    added gitignore whoops

--- routes/superhero.js
+++ routes/superhero.js
@@ -1,15 +1,16 @@
 import { Router } from "express";
-import { getSuperheros, getSuperhero, createSuperhero, updateSuperhero, deleteSuperhero } from "../controllers/superheros.js";
+import { getSuperheros, getSuperhero, createSuperhero, updateSuperhero, deleteSuperhero, getByAlignment, getByPublisher } from "../controllers/superheros.js";
 
 
 const router = Router();
 
 router.get("/", getSuperheros);
-router.get("/:id", getSuperhero);
 router.post("/", createSuperhero);
 router.put("/:id", updateSuperhero);
 router.delete("/:id", deleteSuperhero);
-
+router.get("/alignment/:align", getByAlignment);
+router.get("/publisher/:pub", getByPublisher);
+router.get("/name/:name", getSuperhero);
 
 
 export default router;
