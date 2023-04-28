0f8115bd6c3ba69e883e7a623608b3b2dc8a7626
“Lissa <lwarshaw674@gmail.com>
2023-04-28 10:47:16 -0400

    updated superhero data

---
+++ seed/data.js
@@ -0,0 +1,14 @@
+import fetch from 'node-fetch';
+
+async function fetchSuperheroData() {
+  try {
+    const response = await fetch('https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json');
+    const data = await response.json();
+    console.log(`${data.length} superhero documents fetched`);
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export default fetchSuperheroData;
