ba5d19673fe4e22ae2333392bc78e4e46659730b
“Lissa <lwarshaw674@gmail.com>
2023-04-29 10:55:11 -0400

    updated all characters

--- models/Character.js
+++ models/Character.js
@@ -2,56 +2,55 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const powerstatsSchema = new Schema({
-  intelligence: { type: String },
-  strength: { type: String },
-  speed: { type: String },
-  durability: { type: String },
-  power: { type: String },
-  combat: { type: String },
+ intelligence: { type: String },
+ strength: { type: String },
+ speed: { type: String },
+ durability: { type: String },
+ power: { type: String },
+ combat: { type: String },
 });
 
 const appearanceSchema = new Schema({
-  gender: { type: String },
-  race: { type: String },
-  height: [{ type: String }],
-  weight: [{ type: String }],
-  eyeColor: { type: String },
-  hairColor: { type: String },
+ gender: { type: String },
+ race: { type: String },
+ height: [{ type: String }],
+ weight: [{ type: String }],
+ eyeColor: { type: String },
+ hairColor: { type: String },
 });
 
 const biographySchema = new Schema({
-  fullName: { type: String },
-  alterEgos: { type: String },
-  aliases: [{ type: String }],
-  placeOfBirth: { type: String },
-  firstAppearance: { type: String },
-  publisher: { type: String },
-  alignment: { type: String },
+ fullName: { type: String },
+ alterEgos: { type: String },
+ aliases: [{ type: String }],
+ placeOfBirth: { type: String },
+ firstAppearance: { type: String },
+ publisher: { type: String },
+ alignment: { type: String },
 });
 
 const imagesSchema = new Schema({
-  xs: { type: String },
-  sm: { type: String },
-  md: { type: String },
-  lg: { type: String }
+ xs: { type: String },
+ sm: { type: String },
+ md: { type: String },
+ lg: { type: String }
 });
 
-const superheroSchema = new Schema({
-  name: { type: String },
-  powerstats: powerstatsSchema,
-  appearance: appearanceSchema,
-  biography: biographySchema,
-  work: {
-    occupation: { type: String },
-    base: { type: String },
-  },
-  connections: {
-    relatives: { type: String },
-  },
-  images: imagesSchema
+const characterSchema = new Schema({
+ name: { type: String },
+ powerstats: powerstatsSchema,
+ appearance: appearanceSchema,
+ biography: biographySchema,
+ work: {
+   occupation: { type: String },
+   base: { type: String },
+ },
+ connections: {
+   relatives: { type: String },
+ },
+ images: imagesSchema
 })
 
 
-const Superhero = mongoose.model('Superhero', superheroSchema);
-
-export default Superhero;
+const Character = mongoose.model('Character', characterSchema);
+export default Character;
