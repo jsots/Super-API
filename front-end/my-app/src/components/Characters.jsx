75415bef7b6422b87180cd9894a3aa8360e512c5
“Lissa <lwarshaw674@gmail.com>
2023-05-02 09:40:43 -0400

    updates css

--- front-end/my-app/src/components/Characters.jsx
+++ front-end/my-app/src/components/Characters.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-
+import CharacterCard from './CharacterCard';
 
 function Characters(props) {
  const { characters, onSelectCharacter, onAddToFavorites } = props;
-
 
  return (
    <div>
      <h2>Characters</h2>
      {characters && characters.length > 0 ? (
        characters.map((character) => (
-         <div key={character._id}>
+         <div className="character-card" key={character._id}>
            {character.images && character.images.sm ? (
              <img src={character.images.sm} alt={character.name} />
            ) : (
