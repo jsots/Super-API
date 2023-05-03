809afbcf5bd3ccd2c469063488a15b02604d5ee9
“Lissa <lwarshaw674@gmail.com>
2023-05-03 13:14:52 -0400

    added return original

--- connection/connection.js
+++ connection/connection.js
@@ -1,9 +1,7 @@
 import mongoose from "mongoose";
 import chalk from "chalk";
 
-
-let MONGODB_URI = "mongodb+srv://characters:characters@cluster0.xdbncpu.mongodb.net/?retryWrites=true&w=majority"
-
+let MONGODB_URI = "mongodb+srv://characters:characters@cluster0.xdbncpu.mongodb.net/mydatabase"
 
 if (!MONGODB_URI) {
   MONGODB_URI = 'mongodb://localhost:27017/Characters';
@@ -12,23 +10,17 @@ if (!MONGODB_URI) {
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-})
- 
+});
+
 // This is for Model.findByIdAndUpdate method, specifically so that {new: true} is the default
 mongoose.set("returnOriginal", false);
 
-mongoose
- .connect(MONGODB_URI)
- .catch((error) =>
-   console.log("Error connecting to MongoDB: ", error.message)
- );
-
 mongoose.connection.on("disconnected", () =>
- console.log(chalk.bold("Disconnected from MongoDB!"))
+  console.log(chalk.bold("Disconnected from MongoDB!"))
 );
 
 mongoose.connection.on("error", (error) =>
- console.error(chalk.red(`MongoDB connection error: ${error}`))
+  console.error(chalk.red(`MongoDB connection error: ${error}`))
 );
 
 export default mongoose.connection;
