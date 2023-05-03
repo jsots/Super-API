0748916a87b0cc0a19d4a26a7e879c2b08d7d576
“Lissa <lwarshaw674@gmail.com>
2023-05-03 14:51:21 -0400

    updated URI URL fingers crossed

--- connection/connection.js
+++ connection/connection.js
@@ -1,26 +1,15 @@
-import mongoose from "mongoose";
-import chalk from "chalk";
+import mongoose from "mongoose"
+import chalk from "chalk"
 
-let MONGODB_URI = "mongodb+srv://characters:characters@cluster0.xdbncpu.mongodb.net/mydatabase"
+const url = process.env.PROD_MONGODB || "mongodb://127.0.0.1:27017/Characters" // if the first thing doesnt exist, it will defualt to the second thing which is what we normally do
 
-if (!MONGODB_URI) {
-  MONGODB_URI = 'mongodb://localhost:27017/Characters';
-}
+// This is for Model.findByIdAndUpdate method, specifically so that {new: true}
+mongoose.set("returnOriginal", false)
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(url).catch((error) => console.log("Error connecting to MongoDB: ", error.message))
 
-// This is for Model.findByIdAndUpdate method, specifically so that {new: true} is the default
-mongoose.set("returnOriginal", false);
+mongoose.connection.on("disconnected", () => console.log(chalk.bold("Disconnected from MongoDB!")))
 
-mongoose.connection.on("disconnected", () =>
-  console.log(chalk.bold("Disconnected from MongoDB!"))
-);
+mongoose.connection.on("error", (error) => console.error(chalk.red(`MongoDB connection error: ${error}`)))
 
-mongoose.connection.on("error", (error) =>
-  console.error(chalk.red(`MongoDB connection error: ${error}`))
-);
-
-export default mongoose.connection;
+export default mongoose.connection; 
