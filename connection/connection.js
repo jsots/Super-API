a1704b7977c284e591b5fd42c86801874961a399
“Lissa <lwarshaw674@gmail.com>
2023-05-04 07:41:03 -0400

    connection string

--- connection/connection.js
+++ connection/connection.js
@@ -1,34 +1,22 @@
 import mongoose from "mongoose";
 import chalk from "chalk";
 
+const MONGODB_URI = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/Characters";
 
-let MONGODB_URI = "mongodb+srv://characters:characters@cluster0.xdbncpu.mongodb.net/?retryWrites=true&w=majority"
-
-
-if (!MONGODB_URI) {
-  MONGODB_URI = 'mongodb://localhost:27017/Characters';
-}
-
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
- 
-// This is for Model.findByIdAndUpdate method, specifically so that {new: true} is the default
 mongoose.set("returnOriginal", false);
 
+let mongooseConfig = { useNewUrlParser: true, useUnifiedTopology: true };
+
 mongoose
- .connect(MONGODB_URI)
- .catch((error) =>
-   console.log("Error connecting to MongoDB: ", error.message)
- );
+  .connect(MONGODB_URI, mongooseConfig)
+  .catch((error) => console.log("error connecting to MongoDB", error.message));
 
 mongoose.connection.on("disconnected", () =>
- console.log(chalk.bold("Disconnected from MongoDB!"))
+  console.log(chalk.bold("Disconnected from MongoDB"))
 );
 
 mongoose.connection.on("error", (error) =>
- console.error(chalk.red(`MongoDB connection error: ${error}`))
+  console.error(chalk.red(`mongoDB connection error: ${error}`))
 );
 
 export default mongoose.connection;
